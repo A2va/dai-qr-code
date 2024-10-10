f51f24e5937ba85f9ae7de8cbe9de4239e09a3d6
A2va <leresche.antoine@gmail.com>
2024-10-10 10:18:20 +0200

    Fix copyright year

--- src/main/java/ch/heigvd/dai/commands/Root.java
+++ src/main/java/ch/heigvd/dai/commands/Root.java
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2022 Robin Forestier (Forestierr)
+// Copyright (c) 2024 Robin Forestier (Forestierr)
 //                    Antoine Leresche (A2va)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
